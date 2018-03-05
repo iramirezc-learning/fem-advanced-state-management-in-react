9fada9bb879a8b1afc924ab9f0bb5fd75c016f7d
Isaac Ramirez <isaac.ramirez@unosquare.com>
2018-03-05 13:53:25 -0600

    fix not immutable state add todo

--- 5-redux-and-react/jetsetter-undo/src/reducers/app-reducer.js
+++ 5-redux-and-react/jetsetter-undo/src/reducers/app-reducer.js
@@ -17,37 +17,48 @@ import newItemReducer from './new-item-reducer'
 import filterReducer from './filter-reducer'
 import historyReducer from './history-reducer'
 
-function backupState(appState, stateToBackUp) {
-  appState.history.past = [_.cloneDeep(stateToBackUp), ...appState.history.past]
-  appState.history.future = []
+/**
+ * TODO:
+ * I want to create a version with only a linear array
+ * using an index pointer to current state. v2?
+ */
+function backupState(state) {
+  state.history.past = [_.cloneDeep(state.currentState), ...state.history.past]
+  state.history.future = []
 }
 
+/** 
+ * TODO: 
+ * how to optimize new States whitout clonning too much?,
+ * I need to learn more about it
+ */
 export default function (state = {}, action) {
-  const { currentState } = state;
+  const newState = _.cloneDeep(state)
+  const { currentState } = newState;
 
   switch (action.type) {
     case ADD_NEW_ITEM:
     case REMOVE_ITEM:
     case TOGGLE_ITEM:
     case MARK_ALL_AS_UNPACKED:
-      backupState(state, currentState);
-      state.currentState.items = itemsReducer(currentState.items, action)
-      return { ...state };
+      backupState(newState);
+      newState.currentState.items = itemsReducer(currentState.items, action)
+      return newState;
 
     case UPDATE_NEW_ITEM_VALUE:
-      backupState(state, currentState);
-      state.currentState.newItemValue = newItemReducer(currentState.newItemValue, action)
-      return { ...state };
+      backupState(newState);
+      newState.currentState.newItemValue = newItemReducer(currentState.newItemValue, action)
+      return newState;
 
     case UPDATE_PACKED_ITEMS_FILTER:
     case UPDATE_UNPACKED_ITEMS_FILTER:
-      backupState(state, currentState); 
-      state.currentState.filter = filterReducer(currentState.filter, action);
-      return { ...state }
+      backupState(newState);
+      newState.currentState.filter = filterReducer(currentState.filter, action);
+      return newState
 
     case UNDO:
     case REDO:
-      return { ...historyReducer(state, action) };
+      return { ...historyReducer(newState, action) };
 
     default:
       return state;
