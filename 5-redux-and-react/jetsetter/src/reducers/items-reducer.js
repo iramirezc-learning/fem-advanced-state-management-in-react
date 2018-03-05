7517f2c78736e7b9596a1f7ecdbad6175f6d2454
Isaac Ramirez <isaac.ramirez@unosquare.com>
2018-03-04 21:47:05 -0600

    minor fix

--- 5-redux-and-react/jetsetter/src/reducers/items-reducer.js
+++ 5-redux-and-react/jetsetter/src/reducers/items-reducer.js
@@ -1,6 +1,6 @@
 import { ADD_NEW_ITEM, REMOVE_ITEM, TOGGLE_ITEM, MARK_ALL_AS_UNPACKED } from '../constants';
 
-export default function(state = {}, action) {
+export default function(state = [], action) {
   if (action.type === ADD_NEW_ITEM) {
     const item = action.item;
     return [ ...state, item ];
